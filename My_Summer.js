f70702f41b1d23b2cf92adb1fa8ee9127dde4361
colekomo <nicole.komocsar@gmail.com>
2017-07-17 16:49:07 -0400

    Modified July17

--- My_Summer.js
+++ My_Summer.js
@@ -1,4 +1,4 @@
-function comments(){
+/*function comments(){
 
      var user_comments = $('#user_input').val();
    }
@@ -7,4 +7,15 @@ function comments(){
      $("#submit_button").click();
    }
 
+   $(document).ready(setup)*/
+//
+   function comments() {
+      var user_comments = $('#user_input').val();
+      $("#result").append('<div> Your comment: ' + user_comments + '</div>');
+  console.log("got here") }
+
+   function setup() {
+      $("#submit_button").click(comments);
+
+   }
    $(document).ready(setup)
--- Nicole.js
+++ Nicole.js
@@ -1,12 +1,10 @@
-function comments(){
+function comments() {
+   var user_comments = $('#user_input').val();
+   $("#result").append('<div> Your comment: ' + user_comments + '</div>');
+}
 
-     var user_comments = $('#user_input').val();
-   }
-
-   function setup() {
-     $("#submit_button").click();
-   }
-
-   $(document).ready(setup)
+function setup() {
+   $("#submit_button").click(comments);
 
 }
+$(document).ready(setup)
