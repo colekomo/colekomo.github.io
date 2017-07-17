678d6826ca9a23933b1a743a49ea4721da99c87a
colekomo <nicole.komocsar@gmail.com>
2017-07-17 09:55:55 -0400

    New files

---
+++ My_Summer.js
@@ -0,0 +1,10 @@
+function comments(){
+
+     var user_comments = $('#user_input').val();
+   }
+
+   function setup() {
+     $("#submit_button").click();
+   }
+
+   $(document).ready(setup)
---
+++ Nicole.js
@@ -0,0 +1,12 @@
+function comments(){
+
+     var user_comments = $('#user_input').val();
+   }
+
+   function setup() {
+     $("#submit_button").click();
+   }
+
+   $(document).ready(setup)
+
+}
